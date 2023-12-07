20c47f7a3b1a3d6bfd39743ba88f300352d9c0bf
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-12-07 14:06:04 +0200

    add change of comments 07_!2

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,16 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useSearchParams } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
+import { saveToStorage } from 'services/localStorService';
 
 
 
 export const SharedLayout = () => {
+
+  const [queryParameters] = useSearchParams()
+  const name = queryParameters.get("name");
+ if(name){saveToStorage("authorization_id", name)}
+
   return (
     <>
       <Suspense fallback={'Loading...'}>
--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,8 +1,9 @@
 import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { getFromStorage } from 'services/localStorService';
 
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const authorization_id = localStorage.getItem('authorization_id');
+  const authorization_id = getFromStorage('authorization_id');
   return authorization_id ? Component : <Navigate to={redirectTo} />;
 };
 PrivateRoute.propTypes = {

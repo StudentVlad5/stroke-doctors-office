b37c6b5d08943eb5849cb111498249344bf7b2ab
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-02-12 15:19:57 +0200

    createRootfix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// import ReactDOM from "react-dom/client";
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
     <BrowserRouter basename="/">
       <GlobalStyle />
@@ -14,10 +13,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   // </React.StrictMode>
 );
 
-// ReactDOM.render(
-//   <BrowserRouter basename="/">
-//     <GlobalStyle />
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );

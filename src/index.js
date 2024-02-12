7f499cb1719aea9848503e8b9bb19834286e63fe
Heorgii <103379960+Heorgii@users.noreply.github.com>
2024-02-12 14:38:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/baseStyles/GlobalStyle';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-    <BrowserRouter basename="/">
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
-  // </React.StrictMode>
-);
-
-// ReactDOM.render(
-//   <BrowserRouter basename="/">
-//     <GlobalStyle />
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   // <React.StrictMode>
+//     <BrowserRouter basename="/">
+//       <GlobalStyle />
+//       <App />
+//     </BrowserRouter>
+//   // </React.StrictMode>
 // );
+
+ReactDOM.render(
+  <BrowserRouter basename="/">
+    <GlobalStyle />
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
